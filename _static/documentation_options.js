fb63af62463eca526f726de30c1e272026e8f410
FReina <FReina@users.noreply.github.com>
2022-01-28 17:20:44 +0000

    deploy: 8c95634cc49a2690ed525fe6be0b4b02b17a7d3f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.11-25-g898e9fa',
+    VERSION: '1.0.11-64-g8c95634',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
