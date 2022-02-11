45b0f95c3e427bb9dd46a02c3478a569778e14ed
FReina <FReina@users.noreply.github.com>
2022-02-11 12:21:39 +0000

    deploy: 86a4883c5cbdc78d988fec884f4ae1a47163566d

--- minflux/_static/documentation_options.js
+++ minflux/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.11-104-g8c3507e',
+    VERSION: '1.0.11-105-g86a4883',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
